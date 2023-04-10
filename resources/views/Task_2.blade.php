30feb0f6801f747000800a20e99fc083968d3e52
PRABHUKUMAR J <117797773+Prabhukum@users.noreply.github.com>
2023-04-10 18:15:22 +0530

    last commit

--- resources/views/Task_2.blade.php
+++ resources/views/Task_2.blade.php
@@ -16,27 +16,29 @@
 
 <body>
 
-    <section class="vh-100">
-        <div class="container-fluid h-custom">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-           <div class="col-md-9 col-lg-6 col-xl-5">
+
+        <div class="container-fluid ">
+            <div class="card">
+          <div class="row d-flex justify-content-center align-items-center">
+           <div class="col-md-9 col-lg-8 col-xl-5">
+
               <img src="{{asset('assets/download.png')}}"
                 class="img-fluid" alt="Sample image">
             </div>
-            <div class="col-md-8 col-lg-4 col-xl-4">
-              <form>
+              <div class="col-md-8 col-lg-4 col-xl-4">
 
-                <div class="container d-flex flex-column justify-content-lg-center">
-                 <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-center">
+              <form>
+                 <div class="container">
+                 <div class="d-flex flex-row justify-content-center justify-content-lg-center">
                      <img src="{{asset('assets/header.png')}}" alt="">
                     </div>
 
-                <div class="divider d-flex align-items-center justify-content-lg-center my-4">
+                <div class="divider d-flex justify-content-lg-center">
                   <h2 class="sub_head mb-4">Welcome to Acra Lending Broker Portal</h2>
                 </div>
 
                 <!-- Email input -->
-                <div class="form-outline frm mb-4">
+                <div class="form-outline frm">
                     <label class="form-label mail_id" for="form3Example3">Email ID</label>
                     {{-- <input type="email" id="form3Example3" class="form-control form-control-lg"
                     placeholder="Enter a valid email address" /> --}}
@@ -44,7 +46,7 @@
                 </div>
 
                 <!-- Password input -->
-                <div class="form-outline mb-3">
+                <div class="form-outline ">
                     <label class="form-label" for="form3Example4">Password</label>
                   {{-- <input type="password" id="form3Example4" class="form-control form-control-lg"
                     placeholder="Enter password" /> --}}
@@ -53,13 +55,13 @@
 
                 <div class="d-flex justify-content-between align-items-center">
                   <!-- Checkbox -->
-                  <div class="form-check mb-0">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="form2Example3" />
                     <label class="form-check-label" for="form2Example3">
                       Remember me
                     </label>
                   </div>
-                  <a href="#!" class="text-body bg-primary">Forgot password?</a>
+                  <a href="#!" class="text-body" style="text-decoration: none "><b >Forgot Your password?</a>
                 </div>
 
                 <div class="text-center text-lg-start mt-4 pt-2">
@@ -72,6 +74,7 @@
               </form>
             </div>
           </div>
+        </div>
         </div>
 
 
